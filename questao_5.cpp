1bdb92534d221fc42e5b743793a0d504c482df39
Karim Soares Lentz <karim.lentz@estudante.ufla.br>
2025-06-11 11:37:45 -0300

    questao_5.cpp

---
+++ questao_5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	
+	int n1, p1, n2, p2, n3, p3;
+	
+	ifstream entrada("notas.txt");
+	entrada >> n1 >> p1 >> n2 >> p2 >> n3 >> p3;
+	
+	float media;
+	media = ((n1*p1)+(n2*p2)+(n3*p3))/(p1+p2+p3);
+	
+	ofstream saida("final.txt");
+	saida << fixed << setprecision(2) << media << endl;
+	
+	return 0;
+}
