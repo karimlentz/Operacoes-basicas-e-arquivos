b72f328531a58080a398998f55282d0ef144cdbd
Karim Soares Lentz <karim.lentz@estudante.ufla.br>
2025-06-11 12:19:50 -0300

    questao_9.cpp

---
+++ questao_9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	
+	float comp, alt;
+	cin >> comp >> alt;
+	
+	float area;
+	area = comp * alt;
+	
+	float potencia;
+	potencia = area * 18;
+	
+	ofstream saida("potencia.txt");
+	saida << fixed << setprecision(1) << area << " m2" << endl;
+	saida << fixed << setprecision(1) << potencia << " W" << endl;
+	
+	return 0;
+}
